ea8d86f596000d12309fa9dc3220e9f8fd51025f
mstr-Wolf <lohan.chuan123@gmail.com>
2020-10-12 16:26:58 -0300

    changed variable

--- lib/file_actions.py
+++ lib/file_actions.py
@@ -4,8 +4,7 @@ DATETIME_NOW = datetime.now()
 
 def read_file_content():
     with open("lib/boot", "r+") as file_:
-        line = file_.readlines()[0]
-        date = line.split()
+        date = file_.readlines()[0].split()
         if int(date[1]) < DATETIME_NOW.day or int(date[0]) < DATETIME_NOW.month:
             file_.seek(0)
             file_.write("{} {}".format(str(DATETIME_NOW.month), str(DATETIME_NOW.day)))
