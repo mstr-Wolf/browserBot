d19bb0df18fb494d269c104fc83d46953c70ce4d
mstr-Wolf <lohan.chuan123@gmail.com>
2020-10-25 11:21:37 -0300

    removed unused imports

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,2 @@
-from .file_actions import read_file_content, last_date_recovery, DATETIME_NOW
-from .browser_request import open_url
+from .file_actions import read_file_content, last_date_recovery
 from .clockwork import clockwork
