05b40555a5c26e6efb48767b98eef227098dfe67
mstr-Wolf <lohan.chuan123@gmail.com>
2020-12-03 16:50:22 -0300

    First version for 'AttendClass';
    modified:   src/attendClass.py

--- src/attendClass.py
+++ src/attendClass.py
@@ -1,15 +1,29 @@
 from sys import exit
+from time import sleep
 from clockwork import Clockwork
+from selenium import webdriver
+from datetime import timedelta
 
 class AttendClass(Clockwork):
     def __init__(self, code, **kwargs):
         super().__init__(**kwargs)
-        self.MEET_URL = self.assert_meeting(code, 10, 12)
+        self.MEET_URL = self.assert_meeting_code(code, 10, 12)
+        self.driver = webdriver.Chrome()
 
     def execute(self, **kwargs):
-        pass
+        self.driver.get(self.MEET_URL)
+        self.shutdownConnection()
+        return
 
-    def assert_meeting(self, meeting_code, min_len, max_len):
+    def shutdownConnection(self, **kwargs):
+        self._Clockwork__target += timedelta(hours=1, minutes=30)
+        while True:
+            if self.get_time() == self._Clockwork__target:
+                self.driver.close()
+                return
+            sleep(1)
+
+    def assert_meeting_code(self, meeting_code, min_len, max_len):
         try: meeting_code + "STRING_TEST"
         except TypeError:
             print("Meeting code must be string!")
