a5fa2e9492f64161677d511c9eec87c5722d60f1
sourcerer2 <lohan.chuan123@gmail.com>
2021-01-06 22:05:18 -0300

    Keyboard Interrupt handling

--- src/main.py
+++ src/main.py
@@ -7,11 +7,17 @@ from attendClass import GoogleClass, ZoomClass
 if __name__ == "__main__" and len(sys.argv) >= 5:
     sys.argv[1].lower()
     if sys.argv[1] == "meet" or sys.argv[1] == "google":
-        benThe_Clock = GoogleClass(code=sys.argv[2], class_length=float(sys.argv[3]), hour=int(sys.argv[4]), minute=int(sys.argv[5]))
-        benThe_Clock.run()
+        try:
+            benThe_Clock = GoogleClass(code=sys.argv[2], class_length=float(sys.argv[3]), hour=int(sys.argv[4]), minute=int(sys.argv[5]))
+            benThe_Clock.run()
+        except KeyboardInterrupt:
+            sys.exit()
     elif sys.argv[1] == "zoom":
-        benThe_Clock = ZoomClass(code=sys.argv[2], class_length=float(sys.argv[3]), hour=int(sys.argv[4]), minute=int(sys.argv[5]))
-        benThe_Clock.run()
+        try:
+            benThe_Clock = ZoomClass(code=sys.argv[2], class_length=float(sys.argv[3]), hour=int(sys.argv[4]), minute=int(sys.argv[5]))
+            benThe_Clock.run()
+        except KeyboardInterrupt:
+            sys.exit()
 
 elif __name__ == "__main__" and len(sys.argv) < 5:
     print("Execution example:\n\tpython3 main.py [platform] [code] [length] [target hour] [target minute]")
