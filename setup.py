8251b777293478c71fb24ff063e2aa66a224651b
sourcerer2 <lohan.uchsa@protonmail.com>
2021-01-28 15:04:30 -0300

    Setup added!

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="browsetBot-sourcerer2", # Replace with your own username
+    version="1.3.1",
+    author="Lo Han",
+    author_email="lohan.uchsa@protonmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sourcerer2/browserBot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: GNU/Linux, OS Independent",
+    ],
+    python_requires='>=3.6',
+)
