7fd495a6c7068380c9fa8e820e7d935c569adb92
sourcerer2 <lohan.uchsa@protonmail.com>
2021-01-29 17:00:43 -0300

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="browsetBot-sourcerer2", # Replace with your own username
-    version="1.4",
+    name="are2-sourcerer2", # Replace with your own username
+    version="1.4.0.1",
     author="Lo Han",
     author_email="lohan.uchsa@protonmail.com",
-    description="Browser and web automation engine",
+    description="Web & System automation engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sourcerer2/browserBot",
