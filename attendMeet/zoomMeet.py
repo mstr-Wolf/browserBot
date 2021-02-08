e64ebf5cb621d3122a2b90d1d742052485916987
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-08 15:16:34 -0300

    Up to new setter

--- attendMeet/zoomMeet.py
+++ attendMeet/zoomMeet.py
@@ -24,7 +24,8 @@ class ZoomMeet(GoogleMeet):
         self.driver.find_element_by_id("joinBtn").click()
         return
 
-    def set_meeting_code(self, **kwargs):
-        if "https://zoom.us/j/" in kwargs["meeting_code"]:
-            self.meet_url = kwargs["meeting_code"]
-        else: self.meet_url="https://zoom.us/j/%s" % kwargs["meeting_code"]
+    @meet_url.setter
+    def meet_url(self, code):
+        if "https://zoom.us/j/" in code:
+            self.meet_url = code
+        else: self.meet_url="https://zoom.us/j/%s" % code
