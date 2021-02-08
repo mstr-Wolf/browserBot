66f61c857d9991ccb0219255efe5c1d5596ed07e
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-08 13:54:02 -0300

    Classifier updated (again) & build

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
-        "Operating System :: GNU/Linux",
+        "Operating System :: POSIX :: Linux",
     ],
     python_requires='>=3.6',
 )
