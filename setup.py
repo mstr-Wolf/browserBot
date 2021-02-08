9c1b524b7e43efd02d60a742a9ec0b656b7297ee
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-08 13:55:55 -0300

    Project name updated & build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="meet",
+    name="meet-sourcerer0",
     version="1.4.2",
     author="Lo Han",
     author_email="lohan.uchsa@protonmail.com",
