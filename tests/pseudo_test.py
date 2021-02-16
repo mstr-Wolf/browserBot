1eadd24600c72694c538be0d768a6706ed76125d
sourcerer0 <lohan.uchsa@protonmail.com>
2021-02-16 18:16:43 -0300

    Inheritance from Driver added!

--- tests/pseudo_test.py
+++ tests/pseudo_test.py
@@ -11,13 +11,23 @@ login_data = {}
 
 # SET WEB DRIVER
 adm_driver = Driver(auto_start = True)
-web_driver = Driver(auto_start = True)
+
+
+
+
+# TESTING START FUNCTION
+adm_test = GoogleLogin()
+for _ in range(2):
+        adm_test.start()
+adm_test.driver.close()
+
+
 
 
 # LOGIN 1
-adm_login = GoogleLogin(driver = adm_driver)
-adm_create = CreateGoogle(driver = adm_driver)
-adm_meet = GoogleMeet(driver = adm_driver)
+adm_login = GoogleLogin(driver = adm_driver.driver)
+adm_create = CreateGoogle(driver = adm_driver.driver)
+
 
 adm_login.login_url = "google"
 # myAccount.login_url = "zoom"
@@ -26,6 +36,9 @@ adm_login.login_data = login_data
 if adm_login.doLogin(): adm_create.new_meet()
 
 """
+adm_meet = GoogleMeet()
+adm_meet.driver = adm_driver
+
 if adm_login.doLogin() and adm_create.new_meet():
         adm_meet.set_meeting_url(adm_create.code)
         adm_meet.goto_meet()
@@ -37,8 +50,11 @@ else:
 
 
 # LOGIN 2
-myAccount = GoogleLogin(driver = web_driver)
-meet = GoogleMeet(driver = web_driver)
+myAccount = GoogleLogin()
+myAccount.start()
+
+meet = GoogleMeet()
+meet.driver = myAccount.driver
 
 myAccount.login_url = "google"
 # myAccount.login_url = "zoom"
@@ -54,5 +70,6 @@ else:
 
 
 # CLOSE CONNECTION
-for _ in range(30): sleep(1)
-web_driver.driver.close()
+print("Closing process initiated!")
+for _ in range(180): sleep(1)
+myAccount.driver.close()
